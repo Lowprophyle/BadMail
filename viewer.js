bf6e6fd05eda2cf2c138182be7980ad6d3841a11
Raymond1191 <raymondevonhiles1111@gmail.com>
2017-11-18 02:52:28 +0800

    Added link functionality

--- viewer.js
+++ viewer.js
@@ -1,10 +1,18 @@
 const {shell, remote} = require('electron')
 const webview = document.querySelector('webview')
+const url = require('url')
 
 webview.addEventListener('new-window', (e) => {
-  const protocol = require('url').parse(e.url).protocol
+  const protocol = url.parse(e.url).protocol
   if (protocol === 'http:' || protocol === 'https:') {
     shell.openExternal(e.url)
   }
 })
 
+const win = remote.getCurrentWindow()
+win.addListener('new-window', (e) => {
+    const protocol = url.parse(e.url).protocol
+    if (protocol === 'http:' || protocol === 'https:') {
+        shell.openExternal(e.url)
+    }
+})

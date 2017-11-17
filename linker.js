a66b2c1a9fc4595aa06d16589a8016de16de0086
Raymond1191 <raymondevonhiles1111@gmail.com>
2017-11-18 02:45:48 +0800

    Update

---
+++ linker.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $('webview').hide();
+    $('#web1').show();
+    $("#link1").click(function () {
+        $('#web1').show();
+        $('#web2').hide();
+        $('#web3').hide();
+        $('#web4').hide();
+        $('#web5').hide();
+        $('#web6').hide();
+        $('#web7').hide();
+    });
+    $("#link2").click(function () {
+        $('#web2').show();
+        $('#web1').hide();
+        $('#web3').hide();
+        $('#web4').hide();
+        $('#web5').hide();
+        $('#web6').hide();
+        $('#web7').hide();
+    });
+    $("#link3").click(function () {
+        $('#web3').show();
+        $('#web1').hide();
+        $('#web2').hide();
+        $('#web4').hide();
+        $('#web5').hide();
+        $('#web6').hide();
+        $('#web7').hide();
+    });
+    $("#link4").click(function () {
+        $('#web4').show();
+        $('#web1').hide();
+        $('#web2').hide();
+        $('#web3').hide();
+        $('#web5').hide();
+        $('#web6').hide();
+        $('#web7').hide();
+    });
+    $("#link5").click(function () {
+        $('#web5').show();
+        $('#web1').hide();
+        $('#web2').hide();
+        $('#web3').hide();
+        $('#web4').hide();
+        $('#web6').hide();
+        $('#web7').hide();
+    });
+    $("#link6").click(function () {
+        $('#web6').show();
+        $('#web1').hide();
+        $('#web2').hide();
+        $('#web3').hide();
+        $('#web4').hide();
+        $('#web5').hide();
+        $('#web7').hide();
+    });
+    $("#link7").click(function () {
+        $('#web7').show();
+        $('#web1').hide();
+        $('#web2').hide();
+        $('#web3').hide();
+        $('#web4').hide();
+        $('#web5').hide();
+        $('#web6').hide();
+    });
+
+});
---
+++ viewer.js
@@ -0,0 +1,10 @@
+const {shell, remote} = require('electron')
+const webview = document.querySelector('webview')
+
+webview.addEventListener('new-window', (e) => {
+  const protocol = require('url').parse(e.url).protocol
+  if (protocol === 'http:' || protocol === 'https:') {
+    shell.openExternal(e.url)
+  }
+})
+
